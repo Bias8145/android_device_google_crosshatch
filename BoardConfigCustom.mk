3b74bd63bbcf95a8d4175b47c8e3b809636ef094
Khaliq <bias.khaliq@gmail.com>
2026-04-26 08:03:26 +0700

    Clean up BoardConfigCustom.mk by removing unused entries
    Removed device manifest and SELinux policy directories.

--- BoardConfigCustom.mk
+++ BoardConfigCustom.mk
@@ -21,10 +21,6 @@ TARGET_KERNEL_LLVM_BINUTILS := false
 TARGET_KERNEL_SOURCE := kernel/google/msm-4.9
 TARGET_NEEDS_DTBOIMAGE := true
 
-# Manifests
-DEVICE_MANIFEST_FILE += device/google/crosshatch/lineage_manifest.xml
-DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += vendor/derp/config/device_framework_matrix.xml
-
 # Partitions
 AB_OTA_PARTITIONS += \
     vendor
@@ -42,10 +38,6 @@ BOARD_SYSTEMIMAGE_PARTITION_RESERVED_SIZE := 788398080
 BOARD_SYSTEM_EXTIMAGE_EXTFS_INODE_COUNT := -1
 BOARD_SYSTEM_EXTIMAGE_PARTITION_RESERVED_SIZE := 92160000
 endif
-
-# SELinux
-BOARD_SEPOLICY_DIRS += device/google/crosshatch/sepolicy-lineage/dynamic
-BOARD_SEPOLICY_DIRS += device/google/crosshatch/sepolicy-lineage/vendor
 
 # Verified Boot
 BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 3
